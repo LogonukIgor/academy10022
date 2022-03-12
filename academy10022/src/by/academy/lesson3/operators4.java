2037dfec63ba2e854cf9d75a741b1687c64a0ced
igor <igor@192.168.0.134>
2022-03-12 18:05:41 +0300

    tasks operators

--- academy10022/src/by/academy/lesson3/operators4.java
+++ academy10022/src/by/academy/lesson3/operators4.java
@@ -2,24 +2,26 @@ package by.academy.lesson3;
 
 public class operators4 {
 	public static void main(String[] args) {
-		int s = 370_000;
+		int s = 1_370_000;
 		int sec;
 		int m;
 		int min;
 		int h;
+		int hour;
 		int d;
 		int day;
-		int mo;
-		int month;
-		int y;
-		int year;
-		
+		int w;
+		int week;
+
 		sec = s % 60;
 		m = (s - sec) / 60;
 		min = m % 60;
 		h = (m - min) / 60;
-		
-		
-		System.out.println(h + " часов " + min + " минут " + sec + " секунд");
+		hour = h % 24;
+		d = (h - hour) / 24;
+		day = d % 7;
+		w = (d - day) / 7;
+
+		System.out.println(w + " недель " + day + " дней " + hour + " часов " + min + " минут " + sec + " секунд");
 	}
 }
---
+++ academy10022/src/by/academy/tasks/operators1/task3.java
@@ -0,0 +1,10 @@
+package by.academy.tasks.operators1;
+
+public class task3 {
+	public static void main(String[] args) {
+		int i = 4;
+		int j = 3;
+		int k = i * i + j * j;
+		System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);
+	}
+}
