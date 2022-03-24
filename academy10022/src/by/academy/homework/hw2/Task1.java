a7e8de6eef76178fef029d5892add9ee9f8c6f03
igor <logonuk16@mail.ru>
2022-03-24 17:20:12 +0300

    hw2

--- academy10022/src/by/academy/homework/hw2/Task1.java
+++ academy10022/src/by/academy/homework/hw2/Task1.java
@@ -1,5 +1,38 @@
 package by.academy.homework.hw2;
 
-public class task1 {
+import java.util.Scanner;
 
+public class Task1 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Введите первую строку:");
+		String str1 = scanner.nextLine();
+		System.out.println("Введите вторую строку:");
+		String str2 = scanner.nextLine();
+
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		
+		int value1 = 0;
+		int value2 = 0;
+
+		for (int i = 0; i < arr1.length; i++) {
+			value1 += (int)arr1[i];
+		}
+		
+		for (int i = 0; i < arr2.length; i++) {
+			value2 += (int)arr2[i];
+		}
+
+		if (value1 == value2) {
+			System.out.println("Cтроки являются перестановкой друг друга.");
+		} else {
+			System.out.println("Cтроки не являются перестановкой друг друга.");
+		}
+
+		scanner.close();
+	}
 }
