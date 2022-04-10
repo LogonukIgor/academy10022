f7c591d57407c1ae6ce57c4a1d7ca172127bf6ad
User <User@WIN-D7RVO1T6EFT>
2022-04-10 17:37:07 +0300

    oop

--- academy10022/src/by/academy/classwork/lesson7/Shop.java
+++ academy10022/src/by/academy/classwork/lesson7/Shop.java
@@ -6,14 +6,14 @@ public class Shop {
 
 		User buyer = new User(20, "Alex", 65);
 		User seller = new User(21, "Igor", 20);
-		Product[] products = new Product[4];
+		Product[] products = new Product[3];
 
 		Product milk = new Milk(2.5, 3, "A", "1");
 		Product bread = new Bread(2, 1, "B", "1");
 		Product cheese = new Cheese(5, 2, "C", "1");
 		Product butter = new Butter(3, 1, "D", "1");
 
-		Deal deal = new Deal("12:16 Mn", "Minsk", buyer, seller,products);
+		Deal deal = new Deal("12:16 Mn", "Minsk", buyer, seller, products);
 		System.out.println(deal);
 
 		deal.addProduct(milk);
@@ -21,8 +21,9 @@ public class Shop {
 		deal.addProduct(cheese);
 		deal.addProduct(butter);
 		System.out.println(deal);
-		
+
 		deal.removeProduct(1);
 		System.out.println(deal);
+		
 	}
 }
