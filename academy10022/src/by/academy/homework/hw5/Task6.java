07f22af9aed6ce81fd17badc9565eaf632030818
User <User@WIN-D7RVO1T6EFT>
2022-05-04 21:36:48 +0300

    hw5

---
+++ academy10022/src/by/academy/homework/hw5/Task6.java
@@ -0,0 +1,21 @@
+package by.academy.homework.hw5;
+
+import java.util.Random;
+
+public class Task6 {
+
+	public static final Random RANDOM = new Random();
+
+	public static void main(String[] args) {
+
+		try {
+			int[] array = new int[8];
+			for (int i = 0; i < 10; i++) {
+				array[i] = RANDOM.nextInt(11);
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array is to small, expand the array");
+		}
+
+	}
+}
